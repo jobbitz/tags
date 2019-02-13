0b9d44d89d7cc00899ff07dc6e02c71143a9c5b7
job008 <jobstoit@gmail.com>
2019-02-13 06:50:01 +0100

    Export enum error fields

--- enum/enum.go
+++ enum/enum.go
@@ -9,21 +9,21 @@ import (
 
 // Err gets trown if the enum is incorrect
 type Err struct {
-	valid  []string
-	actual interface{}
+	Valid  []string
+	Actual interface{}
 }
 
 // Error is the error implementation of enum error
 func (err Err) Error() string {
-	return fmt.Sprintf(`Invalid enum: %s, posible: %s`, err.actual, strings.Join(err.valid, `, `))
+	return fmt.Sprintf(`Invalid enum: %s, posible: %s`, err.Actual, strings.Join(err.Valid, `, `))
 }
 
 // Validate checks the values of the object if the enums are correct
 func Validate(obj interface{}) error {
 	return tags.Scan(obj, `enum`, func(tagval string, val interface{}) error {
 		err := Err{strings.Split(tagval, `;`), fmt.Sprint(val)}
-		for _, vld := range err.valid {
-			if vld == err.actual {
+		for _, vld := range err.Valid {
+			if vld == err.Actual {
 				return nil
 			}
 		}
