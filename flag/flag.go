e7837259dada9980365c4ba1562785d664027d57
Job Stoit <jobstoit@icloud.com>
2019-06-01 16:05:37 +0200

    add flag tag

---
+++ flag/flag.go
@@ -0,0 +1,42 @@
+package flag
+
+import (
+	"errors"
+	f "flag"
+	"reflect"
+
+	"github.com/jobbitz/strct"
+)
+
+// Unmarshal reads the object and parses the flag value on to that object
+func Unmarshal(obj interface{}) (err error) {
+	return strct.Scan(obj, func(field reflect.StructField, value *reflect.Value) error {
+		tagVal := field.Tag.Get(`flag`)
+
+		switch value.Kind() {
+		case reflect.Bool:
+			x := f.Bool(tagVal, false, ``)
+			f.Parse()
+			value.SetBool(*x)
+
+		case reflect.Float32, reflect.Float64:
+			x := f.Float64(tagVal, 0.0, ``)
+			f.Parse()
+			value.SetFloat(*x)
+
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			x := f.Int64(tagVal, 0, ``)
+			f.Parse()
+			value.SetInt(*x)
+
+		case reflect.String:
+			x := f.String(tagVal, ``, ``)
+			f.Parse()
+			value.SetString(*x)
+
+		default:
+			return errors.New(`value not a parseble flag`)
+		}
+		return nil
+	})
+}
