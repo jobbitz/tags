3bf4a8e39d2913d37da4140a1c9e9f0c66d2b6ae
Job Stoit <jobstoit@Jobs-MacBook-Pro.local>
2020-01-02 13:32:38 +0100

    modify flags, add a parse args method

--- flags/flags.go
+++ flags/flags.go
@@ -6,8 +6,9 @@
 //
 // Create a struct that requires a flag and parse:
 //   type Config struct {
-//       ConnectionString string `flag:"cs"`
-//       Driver           string `flag:"driver"`
+//       ConnectionString string   `flag:"cs"`
+//       Driver           string   `flag:"driver"`
+//       Args             []string `flag:"*"
 //   }
 //
 //   func main() {
@@ -15,24 +16,41 @@
 //
 //       if err := flags.Parse(conf); err != nil {
 //           ...
-//
+// The '*' can be used to parse in all the args
 package flags
 
 import (
 	"os"
 	"reflect"
 	"regexp"
-	"strings"
 
 	"github.com/jobstoit/strct"
+)
+
+var (
+	args    []string
+	argPtrs []*reflect.Value
+	parsed  bool
 )
 
 // Parse gets the tag and adds it to the property if set.
 // Please note that flags overwrite previous values
 func Parse(obj interface{}) error {
+	if !parsed {
+		args = os.Args[1:]
+		parsed = true
+	}
+
+	defer parseArgsToPtr()
+
 	return strct.Scan(obj, func(field reflect.StructField, value *reflect.Value) error {
 		tagVal := field.Tag.Get(`flag`)
 		if tagVal == `` {
+			return nil
+		}
+
+		if tagVal == `*` { // parse args
+			argPtrs = append(argPtrs, value)
 			return nil
 		}
 
@@ -45,20 +63,60 @@ func Parse(obj interface{}) error {
 	})
 }
 
+// Parsed returns if any flags have been parsed
+func Parsed() bool {
+	return parsed
+}
+
+// Args returns the remainder of the arguments
+func Args() []string {
+	return args
+}
+
+func parseArgsToPtr() {
+	for _, ptr := range argPtrs {
+		if ptr != nil &&
+			ptr.Kind() == reflect.Slice {
+			ptr.Set(reflect.ValueOf(args))
+		}
+	}
+}
+
 func matchArg(fl string, isBool bool) string {
-	args := strings.Join(os.Args[1:], ` `)
-	if !regexp.MustCompile(`[\-]{1,2}` + fl).MatchString(args) {
-		return ``
+	reg := regexp.MustCompile(`[\-]{1,2}` + fl)
+	var key, value string
+
+	for i, arg := range args {
+		if reg.MatchString(arg) {
+			key = arg
+			if i+1 < len(args) {
+				value = args[i+1]
+				break
+			}
+		}
 	}
 
-	if isBool {
+	if isBool && !regexp.MustCompile(`^(true|false|0|1|TRUE|FALSE)$`).MatchString(value) {
+		args = remove(args, key)
 		return `true`
 	}
 
-	match := regexp.MustCompile(`[\-]{1,2}` + fl + `(\=|\ )(\w+)`).
-		FindStringSubmatch(args)
-	if len(match) > 2 {
-		return match[2]
+	if value != `` {
+		args = remove(args, key, value)
 	}
-	return ``
+
+	return value
+}
+
+func remove(ssl []string, items ...string) []string {
+	if len(ssl) < 1 || len(items) < 1 {
+		return ssl
+	}
+	for i, s := range ssl {
+		if s == items[0] {
+			ssl = append(ssl[:i], ssl[i+1:]...)
+			break
+		}
+	}
+	return remove(ssl, items[1:]...)
 }
