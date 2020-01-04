ccb3dcfc6f505f1b5437b5e9f1d0437c4e46e1da
Job Stoit <jobstoit@Jobs-MacBook-Pro.local>
2020-01-04 14:19:12 +0100

    add flag example

--- flags/flags_test.go
+++ flags/flags_test.go
@@ -3,6 +3,7 @@
 package flags
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
@@ -48,25 +49,27 @@ func TestParse(t *testing.T) {
 	if len(obj.Args) != 4 {
 		t.Errorf("Args not set: %q\n", obj.Args)
 	}
+
+	mainSet = Set{}
 }
 
-//func ExampleParse() {
-//	// Input: app -p 8080 -driver postgres arg1 arg2 arg3
-//	os.Args = []string{`app`, `-p`, `8080`, `-driver`, `postgres`, `arg1`, `arg2`, `arg3`}
-//
-//	type Config struct {
-//		Port               int      `flag:"p"`
-//		DBConnectionString string   `flag:"cs"`
-//		DBDriver           string   `flag:"driver"`
-//		Args               []string `flag:"*"`
-//	}
-//
-//	c := new(Config)
-//	if err := Parse(c); err != nil {
-//		panic(err)
-//	}
-//
-//	fmt.Printf("port: %d, connection string: '%s', driver: '%s', args: %q\n", c.Port, c.DBConnectionString, c.DBDriver, c.Args)
-//	// Output:
-//	// port: 8080, connection string '', driver: 'postgres', args: ["arg1" "arg2" "arg3"]
-//}
+func ExampleParse() {
+	// Input: app -p 8080 -driver postgres arg1 arg2 arg3
+	os.Args = []string{`app`, `-p`, `8080`, `-driver`, `postgres`, `arg1`, `arg2`, `arg3`}
+
+	type Config struct {
+		Port               int      `flag:"p"`
+		DBConnectionString string   `flag:"cs"`
+		DBDriver           string   `flag:"driver"`
+		Args               []string `flag:"*"`
+	}
+
+	c := new(Config)
+	if err := Parse(c); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("port: %d, connection string: '%s', driver: '%s', args: %q\n", c.Port, c.DBConnectionString, c.DBDriver, c.Args)
+	// Output:
+	// port: 8080, connection string: '', driver: 'postgres', args: ["arg1" "arg2" "arg3"]
+}
